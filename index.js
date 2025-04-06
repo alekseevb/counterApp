ccbc691d4f370024d41d0caafd962697eb4f2823
Алексей <alexseev617@gmail.com>
2025-04-06 21:33:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ buttonCountDown.addEventListener('click', () => {
 	counter = COUNTER_INITIAL_VALUE
 	numberOfPools.innerText = COUNTER_INITIAL_VALUE
 })
+
+//Другое решение
+let INITIAL_VALUE_POOL = 0
+
+class PoolCounter {
+	constructor() {}
+
+	displayNumber() {}
+
+	counterUp() {}
+
+	reset() {}
+}
+
+const buttonCounterUp = document.getElementById('#counterUp')
+const buttonReset = document.getElementById('#counterDown')
+const NumberOfPools = document.getElementById('#NumberOfPools')
+
+buttonCounterUp.addEventListener('click', () => PoolCounter.counterUp)
+buttonReset.addEventListener('click', () => PoolCounter.reset)
